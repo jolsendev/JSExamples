4b28546ab9ff10e3a1d7a39fd154e9df02a9ddab
jamie <jolsendev@gmail.com>
2016-08-28 16:01:35 -0600

    init commit

---
+++ JSExamples/src/JSTestScript.js
@@ -0,0 +1,33 @@
+  var map;
+  var geoJSON;
+  var request;
+  var gettingData = false;
+  var openWeatherMapKey = "cd053ed32f8216bfdfa1574f5d9785cf";
+  //var googleMapApi = "AIzaSyAkhaHJGKBZbWrfJmCv0BQTkmR7IArC7Ko"
+
+  // Make the weather request
+  var getWeather = function() {
+    gettingData = true;
+    var requestString = "api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7"
+                        + "&APPID=" + openWeatherMapKey;
+    request = new XMLHttpRequest();
+    request.onload = proccessResults;
+    request.open("get", requestString, true);
+    request.send();
+
+  };
+
+function testFunction(){
+    var firstIndex, secondIndex, mainIndex;
+    var text = "";
+
+for()
+
+    for(firstIndex = 0; firstIndex < 10; firstIndex++){
+        for(secondIndex = 0; secondIndex < firstIndex ; secondIndex++){
+            text += "*";
+        }
+        text += "</br>";
+    }
+  document.getElementById("demo").innerHTML = text;
+}
